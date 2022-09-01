e2479a09d9c89247b44e43fdcdbf1c9b5c1844c4
Pierre van Zyl <gingerpeer@Pierres-MacBook-Pro.local>
2022-09-01 15:58:09 +0200

    user input and useState used to display name and age

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [name,setName] = useState('')
+  const [age,setAge] = useState(0)
+  const [showData,setShowData] = useState(false)
+  const clickHandler = () => {
+    if(age === 0){
+      return
+    }else{
+      setShowData(true)
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <Text>Please enter your name</Text>
+        <TextInput 
+          placeholder='eg. John Smith...'
+          style={styles.inputField} 
+          onChangeText={(val)=>setName(val)}
+          />
+          <Text>Please enter your age</Text>
+          <TextInput 
+            keyboardType='numeric'
+            placeholder='eg. 22...'
+            style={styles.inputField} 
+            onChangeText={(val)=>setAge(val)}
+            />
+      </View>
+      {showData && <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, World my name is {name} and my age is {age}</Text>
+      </View>}
+      
+      <View style={styles.buttonContainer}>
+        <Button title='See Alias' onPress={clickHandler} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +49,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+ header: {},
+ body: {},
+ boldText: {
+  fontWeight: 'bold',
+  maxWidth: '50%',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textAlign: 'center'
+ },
+ form: {},
+ inputField: {
+  borderWidth: 1,
+  borderColor: '#777',
+  padding: 8,
+  margin: 10,
+  width: 200,
+ },
+ buttonContainer: {},
 });
