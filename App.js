68083926ba71710e3078f2d11d76e7aabfde5f82
Gingerpeer PC <pierrevanzyl777@gmail.com>
2022-09-02 12:20:05 +0200

    few changes ie importing and use of ScrollView and RefreshControl

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { useState } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-
+import { RefreshControl,SafeAreaView, ScrollView, StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+// TouchableOpacity is better than a Button for styling as Buttons cannot be styled
+const wait = (timeout) => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+}
 export default function App() {
   const [name,setName] = useState('')
   const [age,setAge] = useState(0)
   const [showData,setShowData] = useState(false)
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setName('')
+    setAge(0)
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
+
   const clickHandler = () => {
-    if(age === 0){
-      return
-    }else{
+    console.log('\nName: ',name,'\nAge: ',age)
+    if(age !== 0 && name !== ''){
       setShowData(true)
     }
   }
+  useEffect(()=>{
+    if(age === 0){
+      setShowData(false)
+    }
+  },[name,age])
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+      contentContainerStyle={styles.scrollView}
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+        />
+      }
+        >
       <View style={styles.container}>
         <Text>Please enter your name</Text>
         <TextInput 
@@ -30,15 +56,18 @@ export default function App() {
             onChangeText={(val)=>setAge(val)}
             />
       </View>
-      {showData && <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, World my name is {name} and my age is {age}</Text>
-      </View>}
-      
-      <View style={styles.buttonContainer}>
-        <Button title='See Alias' onPress={clickHandler} />
-      </View>
+      {showData && 
+        <View style={styles.header}>
+          <Text style={styles.boldText}>Hello, World my name is {name} and my age is {age}</Text>
+        </View>
+      }
+        
+        <TouchableOpacity style={styles.buttonContainer} onPress={clickHandler}>
+         <Text style={styles.buttonContainerText}>See Alias</Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -46,6 +75,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+   scrollView: {
+    flex: 1,
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -66,5 +101,14 @@ const styles = StyleSheet.create({
   margin: 10,
   width: 200,
  },
- buttonContainer: {},
+ buttonContainer: {
+  backgroundColor: 'grey',
+  padding: 8,
+  borderRadius: 5,
+  marginTop: 12,
+  marginBottom: 20,
+ },
+ buttonContainerText: {
+  color: 'white'
+ },
 });
